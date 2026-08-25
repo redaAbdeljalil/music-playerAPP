6dbf640ae0635036c984f68311f5018d0dd2b37b
redaAbdeljalil <redar9974@gmail.com>
2026-08-25 20:16:07 +0100

    Initial Commit

---
+++ Aura/AuraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuraApp.swift
+//  Aura
+//
+//  Created by Reda on 25/8/2026.
+//
+
+import SwiftUI
+
+@main
+struct AuraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
